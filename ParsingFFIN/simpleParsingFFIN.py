1fdd5c2b0055f75ce9ee98b41f1d137251b95db4
sorokin5578 <harade44>
2020-04-16 16:47:38 +0300

    third commit Get info about stock

--- ParsingFFIN/simpleParsingFFIN.py
+++ ParsingFFIN/simpleParsingFFIN.py
@@ -2,60 +2,51 @@ import requests
 from bs4 import BeautifulSoup as BS
 
 
-def read_all_page(max_page):
-    page = []
-    for x in range(1, max_page + 1):
-        page.append(requests.get("https://ffin.ru/market/directory/data/?PAGEN_1=" + str(x)))
-    return page
-
-
 def return_new_page(num_page):
     return requests.get("https://ffin.ru/market/directory/data/?PAGEN_1=" + str(num_page))
 
 
 def get_link_stock(find_string):
     dict_stock = {}
-    i = 1
-    # page = read_all_page(28)
+    len_str = len(find_string)
     for i in range(1, 29):
-        for r in return_new_page(i):
-            html = BS(r.content, 'html.parser')
-            el = html.select("table.directory-list_table")
-            for str_key in find_string:
-                for a_tag in el[0].select("a", href=True):
-                    if str_key.lower() in a_tag.text.lower():
-                        dict_stock.update({str_key: "https://ffin.ru" + a_tag.get("href")})
-                        continue;
-            print("прочитано " + str(i))
-            i += 1
-            if len(dict_stock) == len(find_string):
+        r = return_new_page(i)
+        html = BS(r.content, 'html.parser')
+        el = html.select("table.directory-list_table")
+        for a_tag in el[0].select("a", href=True):
+            if len(find_string) == 0:
                 break
-        if len(dict_stock) == len(find_string):
+            for str_key in find_string:
+                if str_key.lower() in a_tag.text.lower():
+                    dict_stock.update({str_key: "https://ffin.ru" + a_tag.get("href")})
+                    find_string.remove(str_key)
+                    continue
+        if len(dict_stock) == len_str:
             break
 
     return dict_stock
 
 
 def get_info_stock(link):
+    r = requests.get(link)
     dict_stock = {}
-    print("nen")
-    r = requests.get(link.get("3d systems corp"))
-    dict_stock = {}
+    grow = "down"
     html = BS(r.content, 'html.parser')
     el = html.select("table.td-last-right")
-    print(el)
-    # dict_stock.update({"Последняя сделка": el[0].select_one("#issuer-profile-informer-last").text})
-    # dict_stock.update({"Изменение":
-    #                        {el[0].select_one("#issuer-profile-informer-change").get("class")[0]:
-    #                             el[0].select_one("#issuer-profile-informer-change").text +
-    #                             el[0].select_one("#issuer-profile-informer-pchange").text}})
-    # dict_stock.update({"Цена открытия": el[0].select_one("#issuer-profile-informer-open").text})
-    # dict_stock.update({"Пред. закрытие": el[0].select_one("#issuer-profile-informer-close").text})
+    grow_index = el[0].select_one("td.lastTradeChange").text.replace(u'\n', u'').replace(u' ', u'')
+    if not ("-" in grow_index):
+        grow = "up"
+    dict_stock.update({"Последняя сделка": el[0].select_one("td.lastTradePrice").text.replace(u'\n', u'')})
+    dict_stock.update({"Изменение": {grow: grow_index}})
+    dict_stock.update({"Дата торгов": el[0].select_one("td.lastTradeDate").text.replace(u'\n', u'')})
+    dict_stock.update({"Потенциал роста": el[0].select_one("td.lastTradePotential").text.replace(u'\n', u'')})
+    dict_stock.update({"Рекомендации": el[0].find_all("span", text=True)[0].text})
+    print(dict_stock)
     return dict_stock
 
 
 # print(get_link_stock(["Netflix inc", "3d systems corp"]))
 # print()
-d = get_link_stock(["3d systems corp","Netflix inc"])
+d = get_link_stock(["3d systems corp"])
 print(d)
-# get_info_stock(d)
+get_info_stock("https://ffin.ru/market/directory/data/quotes/32270/")
