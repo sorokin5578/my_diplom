6b12ea3bf826165fc623a348914acf07b826cfae
sorokin5578 <harade44>
2020-04-18 18:03:10 +0300

    5th commit find by ticker(but trouble with index)

--- ParsingFFIN/simpleParsingFFIN.py
+++ ParsingFFIN/simpleParsingFFIN.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup as BS
 
 
@@ -8,19 +9,28 @@ def return_new_page(num_page):
 
 def get_link_stock(find_string):
     dict_stock = {}
+    dict_ticker = {}
+    dict_link = {}
     len_str = len(find_string)
-    for i in range(1, 29):
+    count_tic = 2
+    for i in range(1, 2):
         r = return_new_page(i)
         html = BS(r.content, 'html.parser')
         el = html.select("table.directory-list_table")
+        ticker = el[0].select("td", text=True)
+        count_tic=2
         for a_tag in el[0].select("a", href=True):
+            if not (re.fullmatch(r'([A-Za-z0-9_\&\.\,\'\(\)]+\s?)+', a_tag.text)):
+                continue
             if len(find_string) == 0:
                 break
             for str_key in find_string:
-                if str_key.lower() in a_tag.text.lower():
-                    dict_stock.update({str_key: "https://ffin.ru" + a_tag.get("href")})
+                tic=ticker[count_tic].text.replace(u'\n', u'').replace(u' ', u'')
+                if str_key.lower() in a_tag.text.lower() or str_key.lower() in tic.lower():
+                    dict_stock.update({str_key: ["https://ffin.ru" + a_tag.get("href"),a_tag.text, tic]})
                     find_string.remove(str_key)
                     continue
+            count_tic += 9
         if len(dict_stock) == len_str:
             break
     return dict_stock
@@ -42,11 +52,11 @@ def get_info_stock(link):
     dict_stock.update({"Потенциал роста": el[0].select_one("td.lastTradePotential").text.replace(u'\n', u'')})
     dict_stock.update({"Рекомендации": el[0].find_all("span", text=True)[0].text})
     dict_stock.update({"Тикер": ticker[ticker.find("(") + 1:len(ticker) - 1]})
-    news = html.select("div.widget_cont")
-    for j in range(2, 4):
-        for n in news[j].select("li", id=True):
-            print(n.text)
-        print("-------")
+    # news = html.select("div.widget_cont")
+    # for j in range(2, 4):
+    #     for n in news[j].select("li", id=True):
+    #         print(n.text)
+    #     print("-------")
 
     return dict_stock
 
@@ -56,11 +66,17 @@ def get_yahoo_page(ticker):
 
 
 # print(get_link_stock(["Netflix inc", "3d systems corp"]))
-# print()
-d = get_link_stock(["3d systems corp"])
-# print(d)
-print(get_info_stock("https://ffin.ru/market/directory/data/quotes/32270/"))
-dict_info = get_info_stock(d.get("3d systems corp"))
-print(dict_info)
 
+d = get_link_stock(["DDD"])
+print(d)
+# d = get_link_stock(["Alliance Data Systems", "3d systems corp", "Netflix inc", "Tesla Motors Inc","Zoetis Inc"])
+# print(d.get("Alliance Data Systems"))
+# print(d.get("3d systems corp"))
+# print(d.get("Netflix inc"))
+# print(d.get("Tesla Motors Inc"))
+# print(d.get("Zoetis Inc"))
+# dict_info = get_info_stock(d.get("3d systems corp"))
+# print(dict_info)
+
+# print(get_info_stock("https://ffin.ru/market/directory/data/quotes/32270/"))
 # get_info_stock_yahoo(get_yahoo_page(dict_info.get("Тикер")))
