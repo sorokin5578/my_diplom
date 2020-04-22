a88bd101630436355b04bf8d3deec73189dea7e7
sorokin5578 <harade44>
2020-04-22 15:34:46 +0300

    10th commit pool ok (maybe fix search)

--- PollReq/simplePool.py
+++ PollReq/simplePool.py
@@ -30,10 +30,41 @@ def return_new_page_ffin(i):
 """End Get page"""
 
 
+# def get_link2(arr):
+#     """Get link for stock, which we find"""
+#     dict_stock = {}
+#     link, find_string = arr[0], arr[1]
+#     try:
+#         r = requests.get(link)
+#         html = BS(r.content, 'html.parser')
+#         el = html.select("table.directory-list_table")
+#         ticker = el[0].select("td", text=True)
+#         count_tic = 2
+#         for a_tag in el[0].select("a", href=True):
+#             if len(find_string) == 0:
+#                 break
+#             if not (re.search(r'([A-Za-z0-9_\&\.\,\'\(\)])',
+#                               a_tag.text[0])) or "ETF" in a_tag.text:
+#                 continue
+#             for str_key in find_string:
+#                 tic = ticker[count_tic].text.replace(u'\n', u'').replace(u' ', u'')
+#                 if str_key.lower() == a_tag.text.lower() or str_key.lower() == tic.lower():
+#                     dict_stock.update({str_key: ["https://ffin.ru" + a_tag.get("href"), a_tag.text, tic]})
+#                     find_string.remove(str_key)
+#                     continue
+#             count_tic += 9
+#     except:
+#         get_info_stock({},{})
+#     for key in dict_stock:
+#         get_info_stock(dict_stock.get(key)[0], dict_stock)
+#     # get_info_stock(dict_stock)
+#     # return dict_stock
+
+
 def get_link2(arr):
     """Get link for stock, which we find"""
     dict_stock = {}
-    link, find_string = arr[0], arr[1]
+    link, str_key = arr[0], arr[1]
     try:
         r = requests.get(link)
         html = BS(r.content, 'html.parser')
@@ -41,27 +72,21 @@ def get_link2(arr):
         ticker = el[0].select("td", text=True)
         count_tic = 2
         for a_tag in el[0].select("a", href=True):
-            if len(find_string) == 0:
-                break
             if not (re.search(r'([A-Za-z0-9_\&\.\,\'\(\)])',
                               a_tag.text[0])) or "ETF" in a_tag.text:
                 continue
-            for str_key in find_string:
-                tic = ticker[count_tic].text.replace(u'\n', u'').replace(u' ', u'')
-                if str_key.lower() == a_tag.text.lower() or str_key.lower() == tic.lower():
-                    dict_stock.update({str_key: ["https://ffin.ru" + a_tag.get("href"), a_tag.text, tic]})
-                    find_string.remove(str_key)
-                    continue
+            tic = ticker[count_tic].text.replace(u'\n', u'').replace(u' ', u'')
+            if str_key.lower() == a_tag.text.lower() or str_key.lower() == tic.lower():
+                dict_stock.update({str_key: ["https://ffin.ru" + a_tag.get("href"), a_tag.text, tic]})
+                break
             count_tic += 9
     except:
-        get_info_stock({})
+        get_info_stock({},{})
     for key in dict_stock:
-        get_info_stock(dict_stock.get(key)[0])
-    # get_info_stock(dict_stock)
-    # return dict_stock
+        get_info_stock(dict_stock.get(key)[0], dict_stock)
 
 
-def get_info_stock(link):
+def get_info_stock(link, dict_res):
     """Get info about stock"""
     dict_news = {}
     dict_stock = {}
@@ -88,20 +113,24 @@ def get_info_stock(link):
         dict_news_finviz = get_news_finviz(get_finviz_page(ticker[ticker.find("(") + 1:len(ticker) - 1]))
         dict_news.update(dict_news_finviz)
     except:
-        sout2({}, {})
-    sout2(dict_stock, dict_news)
+        sout2({}, {}, {})
+    sout2(dict_res, dict_stock, dict_news)
     # return [dict_stock, dict_news]
 
 
-def sout2(stock, news):
-    # l=threading.Lock()
-    # l.acquire()
+def sout2(info, stock, news):
+    l = threading.Lock()
+    l.acquire()
+    for key in info:
+        print("Company: " + info.get(key)[1])
+        print("Ticker: " + info.get(key)[2])
+        print("Link: " + info.get(key)[0])
     for key in stock.items():
         print(key)
     for key in news.items():
         print(key)
     print("--------")
-    # l.release()
+    l.release()
 
 
 def get_news_finviz(r):
@@ -148,7 +177,7 @@ def delta_time_for_finviz_date(then):
     now = datetime.datetime.today().strftime("%b-%d-%y %H:%M")
     if int(now[7:9]) - int(then[7:9]) == 0:
         if now[0:3] == then[0:3]:
-            if int(now[4:6]) - int(then[4:6]) < 2:
+            if int(now[4:6]) - int(then[4:6]) < 1:
                 return True
                 # hour_now = int(now[10:12])
                 # if then[15:17] == "PM":
@@ -168,12 +197,15 @@ def main(find_str):
     Запускаем программу
     """
     res = []
-    for i in range(1, 29):
-        res.append([return_new_page_ffin(i), find_string])
+    for key_str in find_string:
+        for i in range(1, 29):
+            res.append([return_new_page_ffin(i), key_str])
     with Pool(20) as p:
         p.map(get_link2, res)
+        # p.apply_async(get_link2, res)
 
 
 if __name__ == "__main__":
     find_string = ["NFLX", "Tesla Motors Inc", "Apple Inc.", "NKE"]
+    # find_string = ["NFLX", "MUR", "MYL", "NKE"]
     main(find_string)
